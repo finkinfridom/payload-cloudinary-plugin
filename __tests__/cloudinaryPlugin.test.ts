fe6cd1e54ac04f8bbe59645c696b68fbe25a528c
Alberto Maghini <albertomaghini@MacBook-Pro-di-Alberto.local>
2022-10-29 17:09:34 +0200

    added tests

--- __tests__/cloudinaryPlugin.test.ts
+++ __tests__/cloudinaryPlugin.test.ts
@@ -1,18 +1,64 @@
+import { FieldBase } from "payload/dist/fields/config/types";
 import { Field } from "payload/types";
-import { cloudinaryPlugin } from "../src"
-import { mapRequiredFields } from "../src/plugins/cloudinaryPlugin"
+import { cloudinaryPlugin } from "../src";
+import {
+  getPartialField,
+  mapRequiredFields,
+} from "../src/plugins/cloudinaryPlugin";
 
 describe("cloudinaryPlugin", () => {
-    it("config with empty collections should not throw exception", () => {
-        const plugin = cloudinaryPlugin();
-        expect(() => plugin({})).not.toThrowError();
-    })
-    // it("mapRequiredFields should not return duplicate items", () => {
-    //     const result = mapRequiredFields([{
-    //         name: "public_id",
-    //     }]);
-    //     expect(result.filter(r => {
-    //         return (r as any).name === "public_id"
-    //     }).length).toBe(1)
-    // })
-})
+  it("config with empty collections should not throw exception", () => {
+    const plugin = cloudinaryPlugin();
+    expect(() => plugin({})).not.toThrowError();
+  });
+  it("getPartialField should enrich string", () => {
+    expect(getPartialField("sample-field")).toHaveProperty(
+      "name",
+      "sample-field"
+    );
+  });
+  describe("mapRequiredFields", () => {
+    it("should not return duplicate items", () => {
+      const result = mapRequiredFields([
+        {
+          name: "public_id",
+        },
+      ]);
+      expect(
+        result.filter((r) => {
+          return (r as any).name === "public_id";
+        })
+      ).toHaveLength(1);
+    });
+    it("should return DEFAULT_REQUIRED_FIELDS", () => {
+      const expected = mapRequiredFields();
+      expect(expected.length).toBe(3);
+      expect(
+        expected.find((item) => (item as FieldBase).name === "public_id")
+      ).not.toBeNull();
+      expect(
+        expected.find(
+          (item) => (item as FieldBase).name === "original_filename"
+        )
+      ).not.toBeNull();
+      expect(
+        expected.find((item) => (item as FieldBase).name === "secure_url")
+      ).not.toBeNull();
+    });
+    it("should set 'number' field on numeric fields", () => {
+      const expected = mapRequiredFields([
+        "height",
+        "width",
+        "size",
+        "other-field",
+      ]);
+      expect(expected.filter((item) => item.type === "number")).toHaveLength(3);
+    });
+    it("should set 'checkbox' field on boolean field", () => {
+      const expected = mapRequiredFields(["isPrivateFile", "other-field"]);
+      expect(expected.filter((item) => item.type === "checkbox")).toHaveLength(
+        1
+      );
+    });
+  });
+});
--- src/plugins/cloudinaryPlugin.ts
+++ src/plugins/cloudinaryPlugin.ts
@@ -1,120 +1,153 @@
 import { UploadApiResponse } from "cloudinary";
 import { Config, Plugin } from "payload/config";
+import { FieldBase } from "payload/dist/fields/config/types";
 import { IncomingUploadType } from "payload/dist/uploads/types";
 import { APIError } from "payload/errors";
-import { Field, CollectionBeforeChangeHook, CollectionAfterDeleteHook, CollectionAfterReadHook } from "payload/types";
+import {
+  Field,
+  CollectionBeforeChangeHook,
+  CollectionAfterDeleteHook,
+  CollectionAfterReadHook,
+} from "payload/types";
 
 import { CloudinaryPluginRequest, PluginConfig } from "../types";
 
 const GROUP_NAME = "cloudinary";
-export const DEFAULT_REQUIRED_FIELDS = [{ name: "public_id", label: "Public ID" }, { name: "original_filename", label: "Original filename" }, { name: "secure_url", label: "URL" }];
+export const DEFAULT_REQUIRED_FIELDS = [
+  { name: "public_id", label: "Public ID" },
+  { name: "original_filename", label: "Original filename" },
+  { name: "secure_url", label: "URL" },
+];
 
 const setCloudinaryField = (inputField: Partial<Field> | string): Field => {
-    const numberField = ["height", "width", "size"];
-    const booleanField = ["isPrivateFile"];
+  const numberField = ["height", "width", "size"];
+  const booleanField = ["isPrivateFile"];
 
-    const field: Partial<Field> = typeof inputField === "string" ? {
-        name: inputField
-    } : inputField;
-    // @ts-ignore
-    const name = field.name;
-    if (numberField.includes(name)) {
-        field.type = "number";
-    } else if (booleanField.includes(name)) {
-        field.type = "checkbox";
-    } else {
-        field.type = "text";
-    }
+  const field: Partial<Field> = getPartialField(inputField);
+  const name = (field as FieldBase).name;
+  if (numberField.includes(name)) {
+    field.type = "number";
+  } else if (booleanField.includes(name)) {
+    field.type = "checkbox";
+  } else {
+    field.type = "text";
+  }
 
-    return field as Field;
+  return field as Field;
 };
 
-
-export const mapRequiredFields = (additionalFields?: Array<Partial<Field> | string>): Field[] => {
-    return (additionalFields || []).concat(DEFAULT_REQUIRED_FIELDS).map((name) => setCloudinaryField(name));
-}
+export const getPartialField = (field: string | Partial<Field>) => {
+  return typeof field === "string"
+    ? {
+        name: field,
+      }
+    : field;
+};
+export const mapRequiredFields = (
+  additionalFields?: Array<Partial<Field> | string>
+): Field[] => {
+  const merge = (additionalFields || []).concat(DEFAULT_REQUIRED_FIELDS);
+  return merge
+    .filter(
+      (item, idx, arr) =>
+        arr.findIndex((itemToFind) => {
+          const partialItem = getPartialField(item) as FieldBase;
+          const partialItemToFind = getPartialField(itemToFind) as FieldBase;
+          return partialItem.name === partialItemToFind.name;
+        }) === idx
+    )
+    .map((name) => setCloudinaryField(name));
+};
 export const beforeChangeHook: CollectionBeforeChangeHook = async (args) => {
-    const file = args.req.files?.file;
-    if (!(file && args.data.filename)) {
-        return;
-    }
-    try {
-        const uploadResponse = await (
-            args.req as CloudinaryPluginRequest
-        ).cloudinaryService.upload(
-            args.data.filename,
-            file.data,
-            args.req.payload,
-            args.req.collection?.config
-        );
-        return {
-            ...args.data,
-            [GROUP_NAME]: uploadResponse,
-        };
-    } catch (e) {
-        throw new APIError(`Cloudinary: ${JSON.stringify(e)}`);
-    }
-}
-export const afterDeleteHook: CollectionAfterDeleteHook = async ({ req, doc }) => {
-    if (!doc[GROUP_NAME]) {
-        return;
-    }
-    try {
-        await (req as CloudinaryPluginRequest).cloudinaryService.delete(
-            (doc[GROUP_NAME] as UploadApiResponse).public_id
-        );
-    }
-    catch (e) {
-        throw new APIError(`Cloudinary: ${JSON.stringify(e)}`);
-    }
-}
-export const afterReadHook: CollectionAfterReadHook = async ({ doc }) => {
-    const newDoc = {
-        ...doc,
-        original_doc: {
-            url: doc.url,
-            filename: doc.filename
-        },
-        url: doc.cloudinary.secure_url,
-        filename: doc.cloudinary.public_id
+  const file = args.req.files?.file;
+  if (!(file && args.data.filename)) {
+    return;
+  }
+  try {
+    const uploadResponse = await (
+      args.req as CloudinaryPluginRequest
+    ).cloudinaryService.upload(
+      args.data.filename,
+      file.data,
+      args.req.payload,
+      args.req.collection?.config
+    );
+    return {
+      ...args.data,
+      [GROUP_NAME]: uploadResponse,
     };
-    return newDoc;
-}
+  } catch (e) {
+    throw new APIError(`Cloudinary: ${JSON.stringify(e)}`);
+  }
+};
+export const afterDeleteHook: CollectionAfterDeleteHook = async ({
+  req,
+  doc,
+}) => {
+  if (!doc[GROUP_NAME]) {
+    return;
+  }
+  try {
+    await (req as CloudinaryPluginRequest).cloudinaryService.delete(
+      (doc[GROUP_NAME] as UploadApiResponse).public_id
+    );
+  } catch (e) {
+    throw new APIError(`Cloudinary: ${JSON.stringify(e)}`);
+  }
+};
+export const afterReadHook: CollectionAfterReadHook = async ({ doc }) => {
+  const newDoc = {
+    ...doc,
+    original_doc: {
+      url: doc.url,
+      filename: doc.filename,
+    },
+    url: doc.cloudinary.secure_url,
+    filename: doc.cloudinary.public_id,
+  };
+  return newDoc;
+};
 const cloudinaryPlugin = (pluginConfig?: PluginConfig) => {
-    return ((incomingConfig: Config): Config => {
-        const config: Config = {
-            ...incomingConfig,
-            collections: (incomingConfig.collections || []).map(collection => {
-                if (!collection.upload) {
-                    return collection;
-                }
-                return {
-                    ...collection,
-                    hooks: {
-                        ...collection.hooks,
-                        beforeChange: [...collection.hooks?.beforeChange || [], beforeChangeHook],
-                        afterDelete: [...collection.hooks?.afterDelete || [], afterDeleteHook],
-                        afterRead: [...collection.hooks?.afterRead || [], afterReadHook],
-                    },
-                    fields: [
-                        ...collection.fields,
-                        {
-                            name: GROUP_NAME,
-                            type: "group",
-                            fields: [...mapRequiredFields(pluginConfig?.cloudinaryFields)],
-                            admin: { readOnly: true },
-                        },
-                    ],
-                    upload: {
-                        ...collection.upload as IncomingUploadType,
-                        adminThumbnail: ({ doc }) => {
-                            return (doc[GROUP_NAME] as UploadApiResponse)?.secure_url;
-                        },
-                    }
-                }
-            })
+  return ((incomingConfig: Config): Config => {
+    const config: Config = {
+      ...incomingConfig,
+      collections: (incomingConfig.collections || []).map((collection) => {
+        if (!collection.upload) {
+          return collection;
         }
-        return config;
-    }) as Plugin
+        return {
+          ...collection,
+          hooks: {
+            ...collection.hooks,
+            beforeChange: [
+              ...(collection.hooks?.beforeChange || []),
+              beforeChangeHook,
+            ],
+            afterDelete: [
+              ...(collection.hooks?.afterDelete || []),
+              afterDeleteHook,
+            ],
+            afterRead: [...(collection.hooks?.afterRead || []), afterReadHook],
+          },
+          fields: [
+            ...collection.fields,
+            {
+              name: GROUP_NAME,
+              type: "group",
+              fields: [...mapRequiredFields(pluginConfig?.cloudinaryFields)],
+              admin: { readOnly: true },
+            },
+          ],
+          upload: {
+            ...(collection.upload as IncomingUploadType),
+            adminThumbnail: ({ doc }) => {
+              return (doc[GROUP_NAME] as UploadApiResponse)?.secure_url;
+            },
+          },
+        };
+      }),
+    };
+    return config;
+  }) as Plugin;
 };
 export default cloudinaryPlugin;
